ce95d5c901bbd091c52491c2e4f41a6fded4a677
aero-DS <33962783+rookieML-IN@users.noreply.github.com>
2023-12-04 00:05:05 +0530

    Removed redundant code from Baselinedf

--- baseline_df_prep.py
+++ baseline_df_prep.py
@@ -27,6 +27,7 @@ class Baselinedf_Reg:
         return self.df[col].str.split(splt_crt, expand=True)
     
     # THROWS VALUEERROR - WORKS IN LOOP
+    @staticmethod
     def det_nanmap_multistr(*sup_cols,tmp_df,base_col):
         """
         This function uses the newly created columns by column split in the previous function, to help in mapping the values
@@ -42,26 +43,36 @@ class Baselinedf_Reg:
 
         Constraints:
             1. The order of the list contents for the sup_cols will determine the value output format.
-            2. The tmp_df needs to be without missing value predictors.
+            2. The tmp_df needs to be created before and needs to be without missing value predictors.
+            3. Dealing with Date Cases only.
 
         """
         # Library
         import pandas as pd
 
-        # Working Dictionary
+        # Determining the Max values for each Index keys
         main_map = dict()
-
         for sup_col in sup_cols: 
-            sup_crosstab = dict(pd.crosstab(index=tmp_df[sup_col], columns=tmp_df[base_col]))
+            sup_crosstab = pd.crosstab(index=tmp_df[base_col], columns=tmp_df[sup_col])
 
-            for ky in sup_crosstab.keys():
-                max_val = max(list(sup_crosstab.get(ky)))
-                max_val_ind = list(sup_crosstab.get(ky)).index(max_val)
-                ind_val = tmp_df[sup_col].unique()[max_val_ind]
+            for ky in sup_crosstab.index:
+                map_val = sup_crosstab.loc[ky].idxmax()
+                
+                if ky not in main_map.keys():
+                    main_map.update({ky:[map_val]})
+                else:
+                    main_map[ky]+=[map_val]
 
-            if ky not in main_map.keys():
-                main_map.update({ky:[ind_val]})
-            else:
-                main_map[ky]+=[ind_val]
+        # Joining the values of the main_map as per the given format
+        join_req = int(input("Do you want the join the values of values? 0/1 : "))
 
-        return main_map
+        if join_req == 1:
+            join_frmt = input("Please enter the Join format for the values : ")
+            fin_dict = dict()
+
+            for ky,vals in main_map.items():
+                fin_dict.update({ky:join_frmt.join(vals)})
+        else:
+            return main_map
+        
+        return fin_dict
